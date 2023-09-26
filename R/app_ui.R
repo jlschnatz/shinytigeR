6d9ff1985f39d8dbf9aa49634b96b93e4e561a95
Schnatz <janluca.schnatz@gmx.de>
2023-09-26 14:16:55 +0200

    generate broad ui design

--- R/app_ui.R
+++ R/app_ui.R
@@ -9,16 +9,56 @@ app_ui <- function(request) {
     # Leave this function for adding external resources
     golem_add_external_resources(),
     # Your application UI logic
-    fluidPage(
-      h1("shinytigeR"),
-      mod_login_author_ui("login_author_1"),
-      mod_select_item_ui("select_item_1"),
-      mod_display_item_ui("display_item_1"),
-      mod_check_item_ui("check_item_1"),
-      mod_response_analysis_ui("response_analysis_1"),
-      mod_progress_dashboard_ui("progress_dashboard_1")
+    bslib::page_navbar(
+      title = tags$b("PsyBSc2 — tigeR"),
+      theme = bslib::bs_theme(
+        version = 5,
+        bootswatch = "minty",
+        base_font = sass::font_google("Open Sans"),
+        heading_font = sass::font_google("Roboto")
+      ),
+      bslib::nav_panel(
+        title = "Übersicht",
+        icon = bsicons::bs_icon("house-fill", size = 15),
+        fluidRow(
+          h3("Willkommen!"),
+          tags$a("Das wird die Übersicht.")
+        )
+        ),
+      bslib::nav_panel(
+        title = "Üben",
+        icon = bsicons::bs_icon("ui-radios", size = 15),
+        fluidRow(
+          h3("Items"),
+          tags$a("Test"),
+          mod_select_item_ui("select_item_1"),
+          mod_display_item_ui("display_item_1"),
+          mod_check_item_ui("check_item_1")
+          )
+        ),
+      bslib::nav_panel(
+        title = "Fortschritt",
+        icon = bsicons::bs_icon("check-circle-fill", size = 15),
+        fluidRow(
+          h3("Fortschritt"),
+          tags$a("Hier der Fortschritt mit Dashboard"),
+          mod_response_analysis_ui("response_analysis_1"),
+          mod_progress_dashboard_ui("progress_dashboard_1")
+          )
+        ),
+      bslib::nav_spacer(),
+      bslib::nav_item(tags$a(bsicons::bs_icon("person", size = 15), "Mein Account", target = "_blank")),
+      bslib::nav_item(tags$a(bsicons::bs_icon("box-arrow-right", size = 15), "Logout", target = "_blank"))
+      )
+    # fluidPage(
+    #   mod_login_author_ui("login_author_1"),
+    #   mod_select_item_ui("select_item_1"),
+    #   mod_display_item_ui("display_item_1"),
+    #   mod_check_item_ui("check_item_1"),
+    #   mod_response_analysis_ui("response_analysis_1"),
+    #   mod_progress_dashboard_ui("progress_dashboard_1")
     )
-  )
+  #)
 }
 
 #' Add external Resources to the Application
--- R/mod_select_item.R
+++ R/mod_select_item.R
@@ -4,28 +4,28 @@
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
-#' @noRd 
+#' @noRd
 #'
-#' @importFrom shiny NS tagList 
+#' @importFrom shiny NS tagList
 mod_select_item_ui <- function(id){
   ns <- NS(id)
   tagList(
- 
+
   )
 }
-    
+
 #' select_item Server Functions
 #'
-#' @noRd 
+#' @noRd
 mod_select_item_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
- 
+
   })
 }
-    
+
 ## To be copied in the UI
 # mod_select_item_ui("select_item_1")
-    
+
 ## To be copied in the server
 # mod_select_item_server("select_item_1")
